dff29010df829c9d1751e0aad931ef8363f06dcc
gutianxiang <q929061738@163.com>
2023-06-30 14:11:37 +0800

    死循环下载通过判断获取img_list为空后证明已经到了最后一页退出

--- python/web_crawler.py
+++ python/web_crawler.py
@@ -15,6 +15,9 @@ version：0.01
 
 version：0.02
 实现多线程去下载图片,效率得到巨大提升
+
+version:0.03
+解决不确定页数问题，直接死循环添加判断条件退出
 '''
 
 #爬取目标网站
@@ -34,6 +37,9 @@ download_flag = True
 #保存图片的列表
 img_list = []
 
+#获取锁
+mutex = threading.Lock()
+
 #线程执行函数：下载图片并保存文件到指定目录
 def download_thread():
     global img_list
@@ -50,27 +56,30 @@ def download_thread():
         #取出img_list中的第一个后，从img_list中删除
         link = img_list[0].replace('-thumb','')
         name = re.sub('.*?/','',link)
-        #加锁
-        with threading.Lock():
+        
+        #修改img_list需要加锁
+        with mutex:
             del img_list[0]
 
+        #先获取图片内容，再创建文件保存
+        res = requests.get(link)
+        
         #创建文件保存图片
         with open(dir_path+'/'+name,'wb') as fd:
-            res = requests.get(link)
             fd.write(res.content)
-            fd.close()
             print("线程%d:%s下载成功!" % (thread_id,name))
 
 #创建线程并开启
-tids = [threading.Thread(target = download_thread) for _ in range(30)]
+tids = [threading.Thread(target = download_thread) for _ in range(20)]
 for tid in tids:
     tid.start()
 
 #循环将每页里的图片链接保存到img_list中
-for i in range(101,121):
+i = 1
+while True:
     #判断img_list中是否还有图片没下载，如果img_list为空，就继续获取
-    while len(img_list) != 0:
-        time.sleep(1)
+    if len(img_list) != 0:
+        continue
 
     print('开始下载第%d页' % i)
 
@@ -78,9 +87,16 @@ for i in range(101,121):
     response = requests.get(url=url+'?page={}'.format(i),headers=headers)
     response.encoding = response.apparent_encoding
     
-    #正则表达式处理，只保留图片链接
-    with threading.Lock():
+    #正则表达式处理，只保留图片链接到img_list，另外修改img_list需要加锁
+    with mutex:
         img_list = re.findall(re.compile('src="(.*?)" alt=".*?"'),response.text)
+    
+    #判断获取到信息，如果img_list获取到为空，就退出循环;否则准备下一页下载
+    if len(img_list) == 0:
+        print('下载到第%d页退出!!!' % i)
+        break
+    else:
+        i += 1
 
 #修改下载状态标志位，线程退出函数里的循环
 download_flag = False
